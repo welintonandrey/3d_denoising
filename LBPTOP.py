9e62d36663df95cb5a348485f87723d5d99f00c4
Tiago <tiagosn92@gmail.com>
2016-02-26 15:00:20 -0300

    add the first version of NLM with LBP-TOP

--- LBPTOP.py
+++ LBPTOP.py
@@ -2,13 +2,14 @@
 
 import numpy as np
 import sys
+import cv2
 
 from bob.ip.base import * # Bob's implementation of LBP-TOP
 
 class LBPTOP:
     def __init__(self, lbpXY, lbpXT, lbpYT):
         self.lbpTop = LBPTop(lbpXY, lbpXT, lbpYT)
-        
+
         self.histSize = self.lbpTop.xy.max_label + \
                         self.lbpTop.xt.max_label + \
                         self.lbpTop.yt.max_label
@@ -16,14 +17,35 @@ class LBPTOP:
     def getHistSize(self):
         return self.histSize
 
+    def getMaxXY(self):
+        return self.lbpTop.xy.max_label
+
+    def getMaxXT(self):
+        return self.lbpTop.xt.max_label
+
+    def getMaxYT(self):
+        return self.lbpTop.yt.max_label
+
     def generateCodes(self, cuboid):
-        shape = tuple(np.subtract(cuboid.shape, (2, 2, 2)))
+        shape = tuple(np.add(cuboid.shape, (0, 2, 2)))
 
-        xy = np.zeros(shape, dtype=np.uint16)
-        xt = np.zeros(shape, dtype=np.uint16)
-        yt = np.zeros(shape, dtype=np.uint16)
+        newCuboid = np.zeros(shape)
 
-        self.lbpTop.process(cuboid, xy, xt, yt)
+        for i in xrange(0, cuboid.shape[0]):
+            newCuboid[i] = cv2.copyMakeBorder(cuboid[i],1,1,1,1,cv2.BORDER_REFLECT_101)
+
+        firstFrame = np.reshape(newCuboid[0], (1,newCuboid[0].shape[0],newCuboid[0].shape[1]))
+        lastFrame = np.reshape(newCuboid[-1], (1,newCuboid[-1].shape[0],newCuboid[-1].shape[1]))
+        newCuboid = np.concatenate((firstFrame,newCuboid,lastFrame))
+
+        print newCuboid.shape
+        print cuboid.shape
+
+        xy = np.zeros(cuboid.shape, dtype=np.uint16)
+        xt = np.zeros(cuboid.shape, dtype=np.uint16)
+        yt = np.zeros(cuboid.shape, dtype=np.uint16)
+
+        self.lbpTop.process(newCuboid, xy, xt, yt)
 
         return {'xy': xy, 'xt': xt, 'yt': yt}
 
--- ParNLMeans3D.py
+++ ParNLMeans3D.py
@@ -7,13 +7,31 @@ import itertools
 import joblib
 from joblib import Parallel, delayed
 
-def processPixel(video, t, i, j, h, halfWindowSize, halfTemplate, gaussian):
+from LBPTOP import *
+from bob.ip.base import LBP
+
+def hist(lbpVideos, ti, tf, ri, rf, ci, cf, sizeXY, sizeXT, sizeYT):
+    d = 0.0001
+
+    hist = np.concatenate((np.histogram(lbpVideos['xy'][ti:tf,ri:rf,ci:cf], sizeXY)[0], \
+                            np.histogram(lbpVideos['xt'][ti:tf,ri:rf,ci:cf], sizeXT)[0], \
+                            np.histogram(lbpVideos['yt'][ti:tf,ri:rf,ci:cf], sizeYT)[0])) + d
+
+    return hist
+
+def processPixel(video, t, i, j, h, halfWindowSize, halfTemplate, gaussian, lbpVideos, sizeXY, sizeXT, sizeYT):
     ws = 2*halfWindowSize+1
     w = np.zeros((ws, ws, ws))
+    wLBP = np.zeros((ws, ws, ws))
 
     pc = video[t - halfTemplate: t + halfTemplate + 1, \
                 i - halfTemplate: i + halfTemplate + 1, \
                 j - halfTemplate: j + halfTemplate + 1]
+    histc = hist(lbpVideos, \
+                    t - halfTemplate, t + halfTemplate + 1, \
+                    i - halfTemplate, i + halfTemplate + 1, \
+                    j - halfTemplate, j + halfTemplate + 1, \
+                    sizeXY, sizeXT, sizeYT)
 
     for tt in xrange(t - halfWindowSize, t + halfWindowSize + 1):
         for ii in xrange(i - halfWindowSize, i + halfWindowSize + 1):
@@ -29,16 +47,33 @@ def processPixel(video, t, i, j, h, halfWindowSize, halfTemplate, gaussian):
                 dist2 = np.sum(((pc-pn)**2)*gaussian)
                 w[tw, iw, jw] = np.exp(-(dist2/(h*h)))
 
+                histn = hist(lbpVideos, \
+                                tt - halfTemplate, tt + halfTemplate + 1, \
+                                ii - halfTemplate, ii + halfTemplate + 1, \
+                                jj - halfTemplate, jj + halfTemplate + 1, \
+                                sizeXY, sizeXT, sizeYT)
+
+                # chi-square dissimilarity measure
+                wLBP[tw, iw, jw] =  np.sum(((histc - histn)**2) / (histc + histn))
+
+    # calc w_LBP
+    hs = np.std(wLBP)
+    wLBP = np.exp(-wLBP/hs)
+    wLBP[halfWindowSize, halfWindowSize, halfWindowSize] = 0.0
+    wLBP[halfWindowSize, halfWindowSize, halfWindowSize] = np.max(wLBP)
+    wLBP = wLBP / np.sum(wLBP)
+
+    # calc w_Intensity
     w[halfWindowSize, halfWindowSize, halfWindowSize] = 0.0
     w[halfWindowSize, halfWindowSize, halfWindowSize] = np.max(w)
-
     w = w / np.sum(w)
+
     neighborhood = video[t - halfWindowSize: t + halfWindowSize + 1, \
                         i - halfWindowSize: i + halfWindowSize + 1, \
                         j - halfWindowSize: j + halfWindowSize + 1]
 
     print 'Pixel (%3d, %3d) processed!!! ' % (i, j)
-    return np.sum(w*neighborhood)
+    return np.sum(w*neighborhood), np.sum(w*wLBP*neighborhood)
 
 class ParNLMeans3D:
     def __init__(self, h = 3, templateWindowSize = 7, searchWindowSize = 21, sigma = 1):
@@ -67,10 +102,21 @@ class ParNLMeans3D:
         ranges = [range(delta, nFrames - delta), range(delta, nRows - delta), range(delta, nCols - delta)]
         coordinates = list(itertools.product(*ranges))
 
+        lbpTop = LBPTOP(LBP(8, uniform=True, rotation_invariant=True), \
+                        LBP(8, uniform=True, rotation_invariant=True), \
+                        LBP(8, uniform=True, rotation_invariant=True))
+        lbpVideos = lbpTop.generateCodes(video)
+        sizeXY = lbpTop.getMaxXY()
+        sizeXT = lbpTop.getMaxXT()
+        sizeYT = lbpTop.getMaxYT()
+
         ncpus = joblib.cpu_count()
-        results = Parallel(n_jobs=ncpus,max_nbytes=2e9)(delayed(processPixel)(video, t, i, j, self.h, halfWindowSize, halfTemplate, gaussian) for t,i,j in coordinates)
+        results = Parallel(n_jobs=ncpus,max_nbytes=2e9)(delayed(processPixel)(video, t, i, j, self.h, halfWindowSize, halfTemplate, gaussian, lbpVideos, sizeXY, sizeXT, sizeYT) for t,i,j in coordinates)
+
+        print results
 
         for idx in xrange(0,len(results)):
-            out[coordinates[idx][0], coordinates[idx][1], coordinates[idx][2]] = results[idx]
+            out[coordinates[idx][0], coordinates[idx][1], coordinates[idx][2]] = results[idx][0]
+            outLBP[coordinates[idx][0], coordinates[idx][1], coordinates[idx][2]] = results[idx][1]
 
-        return out
+        return (out, outLBP)
